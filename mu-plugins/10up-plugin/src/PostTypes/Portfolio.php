76fc8a35d2050b4d010e12a1b8b0944e257daec8
carl-alberto <cgalbert01@gmail.com>
2025-10-15 23:27:35 +0800

    Add the portfolio meta

--- mu-plugins/10up-plugin/src/PostTypes/Portfolio.php
+++ mu-plugins/10up-plugin/src/PostTypes/Portfolio.php
@@ -73,6 +73,22 @@ class Portfolio extends AbstractPostType {
     }
 
     /**
+     * Returns the default supported features for the post type.
+     *
+     * @return array<string>
+     */
+    public function get_supported_features() {
+        return [
+            'title',
+            'editor',
+            'excerpt',
+            'author',
+            'thumbnail',
+        ];
+    }
+
+
+    /**
      * Run any code after the post type has been registered.
      *
      * @return void
@@ -80,4 +96,43 @@ class Portfolio extends AbstractPostType {
     public function after_register() {
         // Register any hooks/filters you need.
     }
+
+	/**
+     * Register the Portfolio post type with block editor support.
+     */
+    public function register() {
+        register_post_type( $this->get_name(), [
+            'label' => $this->get_plural_label(),
+            'public' => true,
+            'show_in_rest' => true,
+            'menu_icon' => $this->get_menu_icon(),
+            'supports' => $this->get_supported_features(),
+            'taxonomies' => $this->get_supported_taxonomies(),
+        ] );
+    }
+
+	/**
+     * Register custom meta fields for Portfolio post type.
+     */
+    public function register_meta_fields() {
+        register_post_meta( 'portfolio', 'company', [
+            'type' => 'string',
+            'description' => 'Related company',
+            'single' => true,
+            'show_in_rest' => true,
+        ] );
+        register_post_meta( 'portfolio', 'duration', [
+            'type' => 'string',
+            'description' => 'Project duration',
+            'single' => true,
+            'show_in_rest' => true,
+        ] );
+        register_post_meta( 'portfolio', 'position', [
+            'type' => 'string',
+            'description' => 'Role or position',
+            'single' => true,
+            'show_in_rest' => true,
+        ] );
+    }
+
 }
