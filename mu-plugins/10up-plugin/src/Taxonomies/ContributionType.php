f55369142102b3a5ba4a785745ea1e9c9759c5f2
carl-alberto <cgalbert01@gmail.com>
2025-10-16 00:50:55 +0800

    Add the contribution type taxonomy

---
+++ mu-plugins/10up-plugin/src/Taxonomies/ContributionType.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Contribution Type Taxonomy
+ *
+ * @package TenUpPlugin
+ */
+
+namespace TenUpPlugin\Taxonomies;
+
+use TenupFramework\Taxonomies\AbstractTaxonomy;
+
+class ContributionType extends AbstractTaxonomy {
+    public function get_name() {
+        return 'contribution_type';
+    }
+    public function get_singular_label() {
+        return esc_html__( 'Contribution Type', 'tenup-plugin' );
+    }
+    public function get_plural_label() {
+        return esc_html__( 'Contribution Types', 'tenup-plugin' );
+    }
+    public function get_object_type() {
+        return [ 'community_contribution' ];
+    }
+    public function can_register() {
+        return true;
+    }
+}
