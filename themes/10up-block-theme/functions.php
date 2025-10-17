246cc0159e62686bec7aef411c45e87b503c3ab8
carl-alberto <cgalbert01@gmail.com>
2025-10-17 16:22:06 +0800

    Testing the portfolio block

--- themes/10up-block-theme/functions.php
+++ themes/10up-block-theme/functions.php
@@ -37,3 +37,57 @@ require_once __DIR__ . '/template-tags.php';
 
 $theme_core = new \TenupBlockTheme\ThemeCore();
 $theme_core->setup();
+
+
+function render_portfolio_block($attributes) {
+	$args = [
+		'post_type' => 'portfolio',
+		'posts_per_page' => $attributes['postsToShow'] ?? 3,
+	];
+	$query = new WP_Query($args);
+	if (!$query->have_posts()) {
+		return '<p>No portfolio items found.</p>';
+	}
+	$output = '<div class="portfolio-block">';
+	while ($query->have_posts()) {
+		$query->the_post();
+		$image = get_the_post_thumbnail(get_the_ID(), 'medium');
+		$title = get_the_title();
+		$desc = get_the_excerpt();
+		$output .= '<div class="portfolio-item">';
+		$output .= $image ? '<div class="portfolio-image">' . $image . '</div>' : '';
+		$output .= '<h3 class="portfolio-title">' . esc_html($title) . '</h3>';
+		$output .= '<div class="portfolio-desc">' . esc_html($desc) . '</div>';
+		$output .= '</div>';
+	}
+	wp_reset_postdata();
+	$output .= '</div>';
+	return $output;
+}
+
+// Enqueue block editor assets for Portfolio Block
+add_action('enqueue_block_editor_assets', function() {
+	wp_enqueue_script(
+		'10up-portfolio-block',
+		get_template_directory_uri() . '/blocks/portfolio-block/index.js',
+		array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-components', 'wp-editor'),
+		TENUP_BLOCK_THEME_VERSION,
+		true
+	);
+	wp_enqueue_style(
+		'10up-portfolio-block-editor',
+		get_template_directory_uri() . '/blocks/portfolio-block/editor.css',
+		array(),
+		TENUP_BLOCK_THEME_VERSION
+	);
+});
+
+// Enqueue frontend styles for Portfolio Block
+add_action('wp_enqueue_scripts', function() {
+	wp_enqueue_style(
+		'10up-portfolio-block-style',
+		get_template_directory_uri() . '/blocks/portfolio-block/style.css',
+		array(),
+		TENUP_BLOCK_THEME_VERSION
+	);
+});
