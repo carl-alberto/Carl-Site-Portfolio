ae1b5b6042ef966b960cbb79662264107e8ab2aa
carl-alberto <cgalbert01@gmail.com>
2025-10-19 04:19:51 +0800

    testing out theme templates

---
+++ themes/10up-block-theme/blocks/example-block/edit.js
@@ -0,0 +1,7 @@
+import { useBlockProps } from '@wordpress/block-editor';
+
+export const BlockEdit = (props) => {
+	const { attributes, setAttributes } = props;
+	const blockProps = useBlockProps();
+	return <div {...blockProps} />;
+};
