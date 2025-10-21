f3cd200d6a0ae30c9bb6627b50b95dcdbdfb6f4e
carl-alberto <cgalbert01@gmail.com>
2025-10-21 19:52:13 +0800

    testing inner block

--- themes/10up-block-theme/blocks/inner-block-test/edit.js
+++ themes/10up-block-theme/blocks/inner-block-test/edit.js
@@ -1,12 +1,31 @@
+// edit.js
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
 
-export const BlockEdit = (props) => {
-	const { attributes, setAttributes } = props;
-	const blockProps = useBlockProps();
-	const innerBlocksProps = useInnerBlocksProps({}, {});
+export const BlockEdit = ( props ) => {
+	const blockProps = useBlockProps( {
+		className: 'hero-block-editor-preview',
+	} );
+
+	const innerBlocksProps = useInnerBlocksProps(
+		{ className: 'hero-block__inner-content' },
+		{
+			// Optional: restrict allowed blocks
+			// allowedBlocks: [ 'core/paragraph', 'core/button' ],
+		}
+	);
+
 	return (
-		<div {...blockProps}>
-			<ul {...innerBlocksProps} />
+		<div { ...blockProps }>
+			<div className="hero-block__background hero-block__background--placeholder">
+				<div className="hero-block__content">
+					<h1 className="hero-block__title">{ __( 'Post Title', 'tenup' ) }</h1>
+					<div className="hero-block__taxonomies">
+						{ __( 'Category, Tag', 'tenup' ) }
+					</div>
+					<div { ...innerBlocksProps } />
+				</div>
+			</div>
 		</div>
 	);
 };
