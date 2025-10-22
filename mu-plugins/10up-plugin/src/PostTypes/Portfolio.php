1fc88b756427289c848fea300d300522fd624cb5
carl-alberto <cgalbert01@gmail.com>
2025-10-22 11:31:15 +0800

    improve portfolio cpt and block

--- mu-plugins/10up-plugin/src/PostTypes/Portfolio.php
+++ mu-plugins/10up-plugin/src/PostTypes/Portfolio.php
@@ -86,92 +86,90 @@ class Portfolio extends AbstractPostType {
 		];
 	}
 
-	/**
-	 * Run any code after the post type has been registered.
-	 *
-	 * @return void
-	 */
-	public function after_register() {
-		// Register 'role' post meta for REST API and block editor support.
-		register_post_meta(
-			'portfolio',
-			'role',
-			array(
-				'show_in_rest'  => true,
-				'single'        => true,
-				'type'          => 'string',
-				'auth_callback' => function () {
-					return current_user_can( 'edit_posts' );
-				},
-			)
-		);
+	public function get_menu_postion(): ?int {
+		return 5;
+	}
+
+	public function get_options(): array {
+		$options                  = parent::get_options();
+		$options['rewrite']       = [
+			'slug'       => 'portfolio',
+			'with_front' => true,
+		];
+		$options['rest_base']     = 'portfolio';
+		$options['template']      = $this->get_custom_blocks();
+		$options['public']        = true;
+		$options['has_archive']   = true;
+		return $options;
 	}
 
 	/**
-	 * Register the Portfolio post type with block editor support.
+	 * Structure the default blocks for the post type.
 	 */
-	public function register(): bool {
-		register_post_type(
-			$this->get_name(),
-			[
-				'label'                 => $this->get_plural_label(),
-				'labels'                => [
-					'name'                  => esc_html__( 'Portfolios', 'tenup-plugin' ),
-					'singular_name'         => esc_html__( 'Portfolio', 'tenup-plugin' ),
-					'add_new'               => esc_html__( 'Add Portfolio', 'tenup-plugin' ),
-					'add_new_item'          => esc_html__( 'Add New Portfolio', 'tenup-plugin' ),
-					'edit_item'             => esc_html__( 'Edit Portfolio', 'tenup-plugin' ),
-					'new_item'              => esc_html__( 'New Portfolio', 'tenup-plugin' ),
-					'view_item'             => esc_html__( 'View Portfolio', 'tenup-plugin' ),
-					'view_items'            => esc_html__( 'View Portfolios', 'tenup-plugin' ),
-					'search_items'          => esc_html__( 'Search Portfolios', 'tenup-plugin' ),
-					'not_found'             => esc_html__( 'No portfolios found', 'tenup-plugin' ),
-					'not_found_in_trash'    => esc_html__( 'No portfolios found in Trash', 'tenup-plugin' ),
-					'parent_item_colon'     => esc_html__( 'Parent Portfolio:', 'tenup-plugin' ),
-					'all_items'             => esc_html__( 'All Portfolios', 'tenup-plugin' ),
-					'archives'              => esc_html__( 'Portfolio Archives', 'tenup-plugin' ),
-					'attributes'            => esc_html__( 'Portfolio Attributes', 'tenup-plugin' ),
-					'insert_into_item'      => esc_html__( 'Insert into portfolio', 'tenup-plugin' ),
-					'uploaded_to_this_item' => esc_html__( 'Uploaded to this portfolio', 'tenup-plugin' ),
-					'featured_image'        => esc_html__( 'Featured image', 'tenup-plugin' ),
-					'set_featured_image'    => esc_html__( 'Set featured image', 'tenup-plugin' ),
-					'remove_featured_image' => esc_html__( 'Remove featured image', 'tenup-plugin' ),
-					'use_featured_image'    => esc_html__( 'Use as featured image', 'tenup-plugin' ),
-					'menu_name'             => esc_html__( 'Portfolios', 'tenup-plugin' ),
-					'filter_items_list'     => esc_html__( 'Filter portfolios list', 'tenup-plugin' ),
-					'items_list_navigation' => esc_html__( 'Portfolios list navigation', 'tenup-plugin' ),
-					'items_list'            => esc_html__( 'Portfolios list', 'tenup-plugin' ),
-					// Add more labels as needed.
-				],
-				'description'           => esc_html__( 'Portfolio custom post type.', 'tenup-plugin' ),
-				'public'                => true,
-				'publicly_queryable'    => true,
-				'exclude_from_search'   => false,
-				'show_ui'               => true,
-				'show_in_menu'          => true,
-				'show_in_nav_menus'     => true,
-				'show_in_admin_bar'     => true,
-				'menu_position'         => 5,
-				'menu_icon'             => $this->get_menu_icon(),
-				'capability_type'       => 'post',
-				'capabilities'          => [],
-				'map_meta_cap'          => true,
-				'hierarchical'          => false,
-				'supports'              => $this->get_supported_features(),
-				'taxonomies'            => $this->get_supported_taxonomies(),
-				'has_archive'           => true,
-				'rewrite'               => [
-					'slug'       => 'portfolio',
-					'with_front' => true,
-				],
-				'query_var'             => true,
-				'can_export'            => true,
-				'show_in_rest'          => true,
-				'rest_base'             => 'portfolio',
-				'rest_controller_class' => 'WP_REST_Posts_Controller',
-				// Add any other options as needed.
-			]
+	public function get_custom_blocks(): array {
+		$block_arrays = array(
+			array( 'tenup/inner-block-test' ),
+			array(
+				'core/columns',
+				array(),
+				array(
+					array(
+						'core/column',
+						array(
+							'layout' => array( 'type' => 'constrained' ),
+						),
+						array(
+							array( 'core/paragraph', array() ),
+							array(
+								'core/heading',
+								array(
+									'content' => 'Project Overview',
+									'level'   => 2,
+								),
+							),
+							// Nested columns
+							array(
+								'core/columns',
+								array(),
+								array(
+									// First empty column
+									array( 'core/column', array(), array() ),
+									// Second column with styled paragraph
+									array(
+										'core/column',
+										array(),
+										array(
+											array(
+												'core/paragraph',
+												array(
+													'style' => array(
+														'typography' => array(
+															'fontSize' => '18px',
+														),
+														'backgroundColor' => 'indigo',
+														'border'          => array(
+															'radius' => '10px',
+														),
+													),
+													'backgroundColor' => 'indigo',
+												),
+											),
+										),
+									),
+								),
+							),
+							array(
+								'core/gallery',
+								array(
+									'columns' => 3,
+									'linkTo'  => 'none',
+								),
+							),
+						),
+					),
+				),
+			),
 		);
-		return true;
+		return $block_arrays;
 	}
 }
--- themes/10up-block-theme/blocks/inner-block-test/edit.js
+++ themes/10up-block-theme/blocks/inner-block-test/edit.js
@@ -2,10 +2,10 @@
 import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
-export const BlockEdit = ( props ) => {
-	const blockProps = useBlockProps( {
+export const BlockEdit = (props) => {
+	const blockProps = useBlockProps({
 		className: 'hero-block-editor-preview',
-	} );
+	});
 
 	const innerBlocksProps = useInnerBlocksProps(
 		{ className: 'hero-block__inner-content' },
@@ -16,14 +16,14 @@ export const BlockEdit = ( props ) => {
 	);
 
 	return (
-		<div { ...blockProps }>
+		<div {...blockProps}>
 			<div className="hero-block__background hero-block__background--placeholder">
 				<div className="hero-block__content">
-					<h1 className="hero-block__title">{ __( 'Post Title', 'tenup' ) }</h1>
+					<h1 className="hero-block__title">{__('Post Title', 'tenup')}</h1>
 					<div className="hero-block__taxonomies">
-						{ __( 'Category, Tag', 'tenup' ) }
+						{__('Category, Tag', 'tenup')}
 					</div>
-					<div { ...innerBlocksProps } />
+					<div {...innerBlocksProps} />
 				</div>
 			</div>
 		</div>
