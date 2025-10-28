39b14d9b342523c132cd2fa3ceb5c914e5648746
carl-alberto <cgalbert01@gmail.com>
2025-10-29 01:50:21 +0800

    updated to be jest compatible

--- themes/10up-block-theme/blocks/featured-cover-block/edit.js
+++ themes/10up-block-theme/blocks/featured-cover-block/edit.js
@@ -1,36 +1,37 @@
+import React from 'react';
 import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
 
 const Edit = ({ attributes }) => {
-	const featuredImageUrl = useSelect((select) => {
-		const editor = select('core/editor');
-		const core = select('core');
+    const featuredImageUrl = useSelect((select) => {
+        const editor = select('core/editor');
+        const core = select('core');
 
-		const featuredImageId = editor.getEditedPostAttribute('featured_media');
-		const media = featuredImageId ? core.getMedia(featuredImageId) : null;
+        const featuredImageId = editor.getEditedPostAttribute('featured_media');
+        const media = featuredImageId ? core.getMedia(featuredImageId) : null;
 
-		return media?.source_url || null;
-	}, []);
+        return media?.source_url || null;
+    }, []);
 
-	const blockProps = useBlockProps({
-		className: 'wp-block-myplugin-featured-cover-block',
-		style: {
-			backgroundImage: featuredImageUrl ? `url(${featuredImageUrl})` : undefined,
-		},
-	});
+    const blockProps = useBlockProps({
+        className: 'wp-block-myplugin-featured-cover-block',
+        style: {
+            backgroundImage: featuredImageUrl ? `url(${featuredImageUrl})` : undefined,
+        },
+    });
 
-	const overlayStyle = {
-		backgroundColor: attributes?.backgroundColor ? undefined : '#172030',
-	};
+    const overlayStyle = {
+        backgroundColor: attributes?.backgroundColor ? undefined : '#172030',
+    };
 
-	return (
-		<div {...blockProps}>
-			<div className="overlay" style={overlayStyle} />
-			<div className="featured-cover-block__inner-content">
-				<InnerBlocks />
-			</div>
-		</div>
-	);
+    return (
+        <div {...blockProps} data-testid="featured-cover-root">
+            <div className="overlay" style={overlayStyle} data-testid="overlay" />
+            <div className="featured-cover-block__inner-content">
+                <InnerBlocks />
+            </div>
+        </div>
+    );
 };
 
 export default Edit;
